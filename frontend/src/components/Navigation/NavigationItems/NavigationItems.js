0b7a94d181b3994c5fd362a1c0ab3089fe320a49
Stepan Savelyev <mail@s-savelyev.ru>
2020-08-31 04:24:12 +0500

    [IMP] Route manager

--- frontend/src/components/Navigation/NavigationItems/NavigationItems.js
+++ frontend/src/components/Navigation/NavigationItems/NavigationItems.js
@@ -5,7 +5,7 @@ import css            from "./NavigationItems.module.css";
 const NavigationItems = ( props ) => (
     <ul className={css.NavigationItems}>
         <NavigationItem link="/pizzas" exact>Pizza</NavigationItem>
-        <NavigationItem link="/order" exact>Order</NavigationItem>
+        <NavigationItem link="/cart" exact>Cart</NavigationItem>
         <NavigationItem link="/login" exact>Log in</NavigationItem>
         <NavigationItem link="/logout" exact>Log out</NavigationItem>
     </ul>
