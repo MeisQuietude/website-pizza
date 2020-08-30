64731e4ad43310978bdf09303753c17752cef8b8
Stepan Savelyev <mail@s-savelyev.ru>
2020-08-30 15:00:34 +0500

    [ADD] Another one pizza as example

--- data/store.js
+++ data/store.js
@@ -1,9 +1,15 @@
 const initialState = {
     pizzas: [
         {
+            key: "pepperoni",
             title: "Pepperoni",
-            price: "12",
-        }
+            description: "The most delicious pizza in the world",
+        },
+        {
+            key: "10cheeses",
+            title: "10 Cheeses",
+            description: "Even more cheeses! Mozzarella, classic chillegini, soft young cheese, smoked Italian cheese mix, blue cheese, Reggianito, Cheddar with Parmegiano and oregano sauce",
+        },
     ],
 };
 
