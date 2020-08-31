b5bd2a568c5ac6cb9fafafd82bd25eeaab03dfd0
Stepan Savelyev <mail@s-savelyev.ru>
2020-08-31 18:01:27 +0500

    [IMP] Component: Checkout; also - currency, delivery

--- frontend/src/containers/Cart/Cart.js
+++ frontend/src/containers/Cart/Cart.js
@@ -3,13 +3,24 @@ import { connect }                                  from "react-redux";
 import CartItems                                    from "../../components/Cart/CartItems/CartItems";
 import { decrementOrderPizza, incrementOrderPizza } from "../../store/actions/cart";
 import { initPizza }                                from "../../store/actions/pizzaBox";
+import Checkout                                     from "../../components/Cart/Checkout/Checkout";
+
+const DELIVERY_COST = 7;
+const CURRENCY = {
+    USD: { label: "$", coef: 1 },
+    EUR: { label: "€", coef: 0.84 },
+};
 
 class Cart extends Component {
-    getSnapshotBeforeUpdate( prevProps, prevState ) {
+    state = {
+        currency: CURRENCY.USD,
+    };
+
+    async getSnapshotBeforeUpdate( prevProps, prevState ) {
         const cart = this.props.cart;
         const data = { cart };
 
-        return fetch("/users/update-cart", {
+        await fetch("/users/update-cart", {
             method: "POST",
             headers: {
                 "Content-Type": "application/json",
@@ -34,6 +45,31 @@ class Cart extends Component {
             });
     }
 
+    calculateDeliveryPrice = () => {
+        return DELIVERY_COST * this.state.currency.coef;
+    };
+
+    calculateOrderPrice = () => {
+        const cart = { ...this.props.cart };
+        const pizzas = [...this.props.pizzas];
+
+        const orderPrice = Object.keys(cart)
+            .map(key => {
+                const price = (pizzas.find(item => item.key === key) || {}).price;
+                const count = cart[key];
+                return price * count * this.state.currency.coef;
+            })
+            .reduce(( current, next ) => current + next, 0);
+
+        return orderPrice;
+    };
+
+    swapCurrency = () => {
+        this.setState({
+            currency: this.state.currency.label === CURRENCY.USD.label ? CURRENCY.EUR : CURRENCY.USD,
+        });
+    };
+
     render() {
         const mapItemsToCart = Object.keys(this.props.cart).map(key => {
             const pizza = this.props.pizzas.find(e => e.key === key) || {};
@@ -47,20 +83,35 @@ class Cart extends Component {
             };
         });
 
+        const deliveryPrice = this.calculateDeliveryPrice();
+        const orderPrice = this.calculateOrderPrice();
+        const totalPrice = orderPrice + deliveryPrice;
+
         return (
             <React.Fragment>
-                {Object.keys(this.props.cart).length ?
-                    <React.Fragment>
-                        <CartItems
-                            data={mapItemsToCart}
-                            cartHandlers={{
-                                add: this.props.addPizzaToCart,
-                                remove: this.props.removePizzaFromCart,
-                            }} />
-                    </React.Fragment>
-                    :
-                    <h1>Your cart is empty</h1>
-                }
+                <div>
+                    {
+                        Object.keys(this.props.cart).length ?
+                            <React.Fragment>
+                                <CartItems
+                                    data={mapItemsToCart}
+                                    cartHandlers={{
+                                        add: this.props.addPizzaToCart,
+                                        remove: this.props.removePizzaFromCart,
+                                    }} />
+                                <Checkout
+                                    deliveryPrice={deliveryPrice}
+                                    orderPrice={orderPrice}
+                                    totalPrice={totalPrice}
+                                    currency={this.state.currency}
+                                    currencyHandler={this.swapCurrency} />
+                            </React.Fragment>
+
+                            :
+                            <h1>Your cart is empty</h1>
+                    }
+                </div>
+
             </React.Fragment>
         );
     }
