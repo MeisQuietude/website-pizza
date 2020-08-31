a375ac68407caee346df4a0ffbeba0245092628f
Stepan Savelyev <mail@s-savelyev.ru>
2020-08-31 15:52:58 +0500

    [FIX] Pizzas is undefined and we have empty cards instead of full

--- frontend/src/containers/Cart/Cart.js
+++ frontend/src/containers/Cart/Cart.js
@@ -2,8 +2,38 @@ import React, { Component }                         from "react";
 import { connect }                                  from "react-redux";
 import CartItems                                    from "../../components/Cart/CartItems/CartItems";
 import { decrementOrderPizza, incrementOrderPizza } from "../../store/actions/cart";
+import { initPizza }                                from "../../store/actions/pizzaBox";
 
 class Cart extends Component {
+    getSnapshotBeforeUpdate( prevProps, prevState ) {
+        const cart = this.props.cart;
+        const data = { cart };
+
+        return fetch("/users/update-cart", {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json",
+            },
+            body: JSON.stringify(data),
+        });
+    }
+
+    componentDidMount() {
+        return fetch(
+            "/pizzas",
+            { method: "GET" },
+        )
+            .then(response => {
+                return response.json();
+            })
+            .then(( response ) => {
+                this.props.initPizza(response.data.pizzas);
+            })
+            .catch(( error ) => {
+                this.setState({ error: error });
+            });
+    }
+
     render() {
         const mapItemsToCart = Object.keys(this.props.cart).map(key => {
             const pizza = this.props.pizzas.find(e => e.key === key) || {};
@@ -47,6 +77,7 @@ const mapDispatchToProps = dispatch => {
     return {
         addPizzaToCart: ( pizzaKey ) => dispatch(incrementOrderPizza(pizzaKey)),
         removePizzaFromCart: ( pizzaKey ) => dispatch(decrementOrderPizza(pizzaKey)),
+        initPizza: ( pizzas ) => dispatch(initPizza(pizzas)),
     };
 };
 
